f6cc2d1c99a26871628ddf521d7aab452b7d89c6
NaveeraRajChuhan <nnfstars@gmail.com>
2026-05-20 11:35:52 +0500

    variab

--- index.cpp
+++ index.cpp
@@ -10,19 +10,52 @@ main()
 }
 
 
-//
+// variable......
+// area jismai hum koi bhi value store kar sakty hain usko variable kehty hain. 
+// variable ko declare karna parta hai. variable ko declare karne ke liye humein uska data type specify karna parta hai.
+//  data type se compiler ko pata chalta hai ke variable mein kis type ki value store hogi. 
+// variable ko declare karne ke baad hum usmein value assign kar sakty hain. 
+// variable ki value ko change bhi kar sakty hain. variable ka naam unique hona chahiye aur usmein space nahi hona chahiye.
+//  variable ka naam aise hona chahiye ke usse samajh mein aaye ke usmein kis type ki value store hogi.
+
+
+
+main()
+{
+    1,2,3,4,5;   //integer variable
+    1.1,2.2,3.3; //float variable
+    'a','b','c'; //character variable
+    "Hello World"; //string variable
+    0,1; //boolean variable
+}
+
+
+// inko store karnay k lie hum variable banatay hain
 
 
 
 
+main()
+{
+    int short long a=10; //integer variable
+    float double   b=1.1; //float variable
+    char c='a'; //character variable
+    string d="Hello World"; //string variable
+    bool e=1; //boolean variable
+
+    // to print the value of variable
+    cout<<a; //output: 10
+
+    return 0;
+}
 
 
+// Arithmetic operators
 
-
-
-
-
-
+main()
+{
+    
+}
 
 
 
