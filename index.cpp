6a8f64850dcb4a81b451f3d8a8567092a5158bd4
NaveeraRajChuhan <nnfstars@gmail.com>
2026-05-20 11:22:40 +0500

    function

--- index.cpp
+++ index.cpp
@@ -1,3 +1,34 @@
 #include <iostream>     //------header file
 #include  <conio.h>     //------header file
 using namespace std;    //-----library
+
+//function
+main()
+{
+    cout<<"Hello World";   //------output statement
+    return 0;           //------return statement
+}
+
+
+//
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
